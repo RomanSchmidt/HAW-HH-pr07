a5750568c55aa7ba448b4fe1291c5739748f68ea
Roman Schmdit <roman.schmidt@haw-schmidt.de>
2018-11-19 08:14:29 +0100

    - working very simple ai

--- player/artifactual.rb
+++ player/artifactual.rb
@@ -3,11 +3,16 @@ require './player/a_player'
 class Artifactual
   include APlayer
 
+  def initialize(type, input, renderer, match)
+    super(type, input, renderer, match)
+    @size = Match::SYMBOLS.length
+  end
+
   def get_code
-    @size = Match::SYMBOLS.size - 1
     Match::SYMBOL_NUMBER.times do
       @symbols_to_guess.push(Match::SYMBOLS[rand(@size)])
     end
+    @symbols_to_guess
   end
 
   protected
